676007732c3d7f5f01a53b436de041da1274933b
Alex Kit <alex.kit@atmajs.com>
2024-12-19 00:54:29 +0100

    use (esm)

--- deploy-cli.sh
+++ deploy-cli.sh
@@ -29,7 +29,7 @@ npx ts-node test/check.ts --pin test
 
 
 echo "Change generation to JavaScript"
-npx 0xweb config --set "settings.generate.target=js;settings.generate.extension=mjs"
+npx 0xweb config --set "settings.generate.target=mjs"
 
 
 echo "Create contracts JavaScript client"
